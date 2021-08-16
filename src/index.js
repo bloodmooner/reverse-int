71c39ab2c7719c36a1ff4a0fb9a7af1d612c06fc
Sergey Goncharuk <fleshed914@gmail.com>
2021-08-16 11:10:10 +0300

    fix: name of parameter in ternar condition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-    return String(num).slice(0, 1) == '-' ? String(n).slice(1).split('').reverse().join('') : String(n).split('').reverse().join('');
+    return String(n).slice(0, 1) == '-' ? String(n).slice(1).split('').reverse().join('') : String(n).split('').reverse().join('');
 };
